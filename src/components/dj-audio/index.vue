8c9fa30f2f67d4da6832c2815ae0e994492f6e86
15623702696 <2458186212@qq.com>
2024-05-11 02:48:45 +0800

    1、更新dj-audio组件播放暂停按钮样式

--- src/components/dj-audio/index.vue
+++ src/components/dj-audio/index.vue
@@ -131,6 +131,16 @@ const progressDragChange = () => {
                         <path fill="currentColor"
                             d="M12 6h-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2zm10 0h-2a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h2a2 2 0 0 0 2-2V8a2 2 0 0 0-2-2z" />
                     </svg>
+                    <!-- <svg v-if="!audioConfig.state" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                        fill="currentColor" class="bi bi-play-fill icon" viewBox="0 0 16 16">
+                        <path
+                            d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                        class="bi bi-pause-fill icon" viewBox="0 0 16 16">
+                        <path
+                            d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
+                    </svg> -->
                 </button>
             </div>
             <div class="dj-audio-progress__wrapper">
