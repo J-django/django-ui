6c377f5751bd9909fecc014ce4b24d46c6d5de77
15623702696 <2458186212@qq.com>
2024-06-14 19:59:00 +0800

    1、优化dj-divider样式

--- src/components/dj-divider/index.vue
+++ src/components/dj-divider/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts" name="dj-divider">
 // plugin
 import "./index.less";
-import { useSlots } from 'vue'
+import { useSlots, PropType } from 'vue'
 
 // script
 const slots = useSlots();
+
+type DividerType = "solid" | "dashed" | "dotted";
+type DividerAlign = "left" | "center" | "right";
 
 defineProps({
     text: {
@@ -12,22 +15,39 @@ defineProps({
         default: ""
     },
     type: {
-        type: String,
+        type: String as PropType<DividerType>,
         default: "solid",
-        validator: (type: string) => {
-            return ["solid", "dashed", "dotted"].includes(type);
+        validator: (type: string): boolean => {
+            return ["solid", "dashed", "dotted"].includes(type as DividerType);
         }
+    },
+    align: {
+        type: String as PropType<DividerAlign>,
+        default: "center"
+    },
+    margin: {
+        type: [Number, String],
+        default: 10
+    },
+    offset: {
+        type: [Number, String],
+        default: 10
     }
 })
 </script>
 
 <template>
     <div class="dj-divider">
-        <div class="dj-divider-inner" :class="['dj-divider-inner--' + type]">
-            <slot v-if="slots['default']" />
-            <template v-else>
-                {{ text }}
-            </template>
+        <div class="dj-divider__wrapper" :class="['dj-divider-type--' + type, align]" :style="{
+            '--dj-divider-margin': typeof offset === 'string' ? `${margin}` : `${margin}px`,
+            '--dj-divider-offset': typeof offset === 'string' ? `${offset}` : `${offset}px`
+        }">
+            <span class="dj-divider__inner">
+                <slot v-if="slots['default']" />
+                <template v-else>
+                    {{ text }}
+                </template>
+            </span>
         </div>
     </div>
 </template>
