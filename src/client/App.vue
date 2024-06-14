1ed0d2c219f9c2fcabf77caf19c960a0704a3706
15623702696 <2458186212@qq.com>
2024-06-14 20:01:18 +0800

    1、优化各个组件disabled样式

--- src/client/App.vue
+++ src/client/App.vue
@@ -7,15 +7,27 @@ import VideoSrc from '@/assets/video/Blue_Sky_and_Clouds_Timelapse_0892__Videvo_
 // script
 const accordion = ref(false);
 const content = ref();
+const accordionDisabled = ref(true);
 
 const switchValue = ref("apple");
 const switchArray = ref(["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew"])
+
+const toggleAccordion = () => {
+    accordion.value = !accordion.value;
+}
+
+const toggleDisabled = () => {
+    accordionDisabled.value = !accordionDisabled.value;
+}
 </script>
 
 <template>
     <div class="layout">
         <div class="line">
-            <dj-switch :data="switchArray" v-model="switchValue" />
+            <dj-divider align="left" type="dashed" text="New Divider" :offset="10" />
+        </div>
+        <div class="line">
+            <dj-segmented :data="switchArray" v-model="switchValue" disabled />
         </div>
         <div class="line">
             <dj-button>Default</dj-button>
@@ -65,7 +77,9 @@ const switchArray = ref(["apple", "banana", "cherry", "date", "elderberry", "fig
             <dj-divider text="New Divider" type="dotted" />
         </div>
         <div class="line">
-            <dj-accordion v-model="accordion" title="发现更美好的自己，追寻内心的光芒"
+            <dj-button style="margin-bottom: 8px;" @click="toggleAccordion">toggle accordion</dj-button>
+            <dj-button style="margin-bottom: 8px;" @click="toggleDisabled">toggle disabled</dj-button>
+            <dj-accordion v-model="accordion" :disabled="accordionDisabled" title="发现更美好的自己，追寻内心的光芒"
                 content="在喧嚣的世界中，我们常常迷失了自己，远离了内心的声音。然而，每个人都有一颗闪耀的星，它蕴藏着无限的潜力和独特的魅力。现在，是时候发现更美好的自己，追寻内心的光芒了。每个人都有自己的故事，每个人都有自己的梦想。不要害怕展示真实的自我，敢于追求内心的渴望。不论是小小的爱好还是大胆的目标，都值得我们用心去追寻。让自己的内在力量得到释放，成为引领自己命运的明灯。时光匆匆，岁月如梭，我们不应停滞不前。在这个瞬息万变的世界，保持学习和成长的态度是关键。与其追逐瞬时的浮华，不如从内心汲取力量，拥抱真实的自己。唯有不断地挑战自我，我们才能真正发现自己的潜能和价值。生活中，我们常常受到外界的评判和期待，这让我们迷失了自我。然而，真正的力量来源于内心。当我们坚定地追随内心的声音，才能找到真正属于自己的道路。不要害怕追求与众不同，与众不同才是独特的标志，是引领自己脱颖而出的钥匙。让我们勇敢地面对内心的声音，追寻内心的光芒。发现更美好的自己，踏上属于自己的璀璨之路。相信自己的梦想，坚持自己的信念，无论前路如何曲折，我们都能够闪耀出属于自己的光芒。因为，内心的力量是永恒不变的，它将引领我们走向更美好的未来。"></dj-accordion>
         </div>
         <div class="line">
