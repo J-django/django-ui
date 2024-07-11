900650a8641a3a5d5b6ba5c5434f7c6ea5e1ffd3
15623702696 <2458186212@qq.com>
2024-07-12 00:21:41 +0800

    1、去除vconsole

--- src/client/main.ts
+++ src/client/main.ts
@@ -1,8 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import DjangoUI from '@/index';
-import VConsole from 'vconsole';
-new VConsole();
 
 const app = createApp(App);
 app.use(DjangoUI)
